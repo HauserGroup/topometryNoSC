a320f566a52aaedc6d188c85ce26e5273aed2b31
jsture <jakobsturemadsen@gmail.com>
2026-06-09 18:39:25 +0200

    fixed spectral eigen

--- src/topo/spectral/__init__.py
+++ src/topo/spectral/__init__.py
@@ -1,6 +1,13 @@
-"""Spectral operators and eigendecomposition."""
+"""Spectral graph operators and eigendecomposition utilities."""
 
-from ._spectral import LE, degree, diffusion_operator, graph_laplacian
+from ._spectral import (
+    LE,
+    degree,
+    degree_matrix,
+    degree_vector,
+    diffusion_operator,
+    graph_laplacian,
+)
 from .eigen import EigenDecomposition, eigendecompose
 
 __all__ = [
@@ -8,6 +15,8 @@ __all__ = [
     "diffusion_operator",
     "LE",
     "degree",
+    "degree_vector",
+    "degree_matrix",
     "EigenDecomposition",
     "eigendecompose",
 ]
